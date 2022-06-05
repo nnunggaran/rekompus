cf167ef43d20c77923661c95bc02b4e9d4054356
rulhuda <nurulhuda.unhasy@gmail.com>
2022-06-06 00:30:10 +0700

    Menambahkan routing untuk dashboard-admin

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -3,6 +3,14 @@ import ListKampus from '../views/page/list-kampus';
 import DetailKampus from '../views/page/detail-kampus';
 import EditProfileUser from '../views/page/edit-profile-user';
 import DashboardUser from '../views/page/dashboard-user';
+import DashboardAdmin from '../views/page/dashboard-admin';
+import AddKampus from '../views/page/add-kampus';
+import EditKampus from '../views/page/edit-kampus';
+import EditJurusan from '../views/page/edit-jurusan';
+import EditProfileAdmin from '../views/page/edit-profil-admin';
+import InfoKampus from '../views/page/info-kampus';
+import InfoJurusan from '../views/page/info-jurusan';
+import SettingJurusan from '../views/page/setting-jurusan';
 
 const routes = {
   '/': Homepage,
@@ -11,6 +19,14 @@ const routes = {
   '/kampus/:id': DetailKampus,
   '/dashboard/:id': DashboardUser,
   '/edit-profile/:id': EditProfileUser,
+  '/dashboard-admin/:id': DashboardAdmin,
+  '/edit-profile-admin/:id': EditProfileAdmin,
+  '/add-kampus': AddKampus,
+  '/edit-jurusan/:id': EditJurusan,
+  '/info-kampus/:id': InfoKampus,
+  '/info-jurusan/:id': InfoJurusan,
+  '/edit-kampus/:id': EditKampus,
+  '/setting-jurusan/:id': SettingJurusan,
 };
 
 export default routes;
